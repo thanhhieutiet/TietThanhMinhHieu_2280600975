d24a5ee56287d3970dba2d05efcc14c3535833d9
thanhhieutiet <thanhhieutiet@gmail.com>
2025-03-10 21:34:53 +0700

    [add] lab-02 railfence

--- Lab-02/ex01/api.py
+++ Lab-02/ex01/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
+from cipher.railfence import RailFenceCipher
 
 app = Flask(__name__)
 
@@ -42,6 +43,25 @@ def vigenere_decrypt():
     key = data["key"]
     decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
     return jsonify({"decrypted_message": decrypted_text})
+
+
+#RAILFENCE CIPHER
+railfence_cipher = RailFenceCipher()
+@app.route("/api/railfence/encrypt", methods=["POST"])
+def encrypt():
+    data = request.json
+    plain_text = data["plain_text"]
+    key = int(data["key"])
+    encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text,key)
+    return jsonify({"encrypted_text": encrypted_text})
+
+@app.route("/api/railfence/decrypt", methods=["POST"])
+def decrypt():
+    data = request.json
+    cipher_text = data["cipher_text"]
+    key = int(data["key"])
+    decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text,key)
+    return jsonify({"decrypted_text": decrypted_text})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
